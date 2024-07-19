0622eb9d16db94be422fe5b8e56c70aa26411d27
Araon <soumik.ghosh1998@gmail.com>
2024-07-20 05:26:38 +0530

    updated readme

--- script/load_data.py
+++ script/load_data.py
@@ -2,28 +2,22 @@ import pymongo
 import requests
 import time
 
-# Wait for MongoDB to start
-time.sleep(10)
+time.sleep(5)
 
-# MongoDB connection
 client = pymongo.MongoClient("mongo:27017")
 db = client["anime_db"]
 collection = db["anime_collection"]
 
-# URL of the JSON file
 url = "https://github.com/manami-project/anime-offline-database/raw/" \
     "master/anime-offline-database.json"
 
-# Fetching the JSON data from the URL
 response = requests.get(url)
 data = response.json()
 
-# Insert data into MongoDB collection
 if "data" in data:
     collection.insert_many(data["data"])
     print(f"Inserted {len(data['data'])} documents into the collection")
 else:
     print("No data found in the JSON file")
 
-# Verify insertion
 print(f"Total documents in collection: {collection.count_documents({})}")
